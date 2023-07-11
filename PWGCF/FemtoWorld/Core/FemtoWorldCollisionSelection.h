e1141237b66a6cb0a51b2746fbcae8100d5a8248
Zuzanna Chochulska <87480906+zchochul@users.noreply.github.com>
2023-07-11 15:45:44 +0200

    Update FemtoWorldCollisionSelection.h

--- PWGCF/FemtoWorld/Core/FemtoWorldCollisionSelection.h
+++ PWGCF/FemtoWorld/Core/FemtoWorldCollisionSelection.h
@@ -1,6 +1,6 @@
 // Copyright 2019-2020 CERN and copyright holders of ALICE O2.
-// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
-// All rights not expressly granted are reserved.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright
+// holders. All rights not expressly granted are reserved.
 //
 // This software is distributed under the terms of the GNU General Public
 // License v3 (GPL Version 3), copied verbatim in the file "COPYING".
@@ -10,15 +10,16 @@
 // or submit itself to any jurisdiction.
 
 /// \file FemtoWorldCollisionSelection.h
-/// \brief FemtoWorldCollisionSelection - event selection within the o2femtoworld framework
-/// \author Andi Mathis, TU München, andreas.mathis@ph.tum.de
-/// \author Zuzanna Chochulska, WUT Warsaw, zchochul@cern.ch
+/// \brief FemtoWorldCollisionSelection - event selection within the
+/// o2femtoworld framework \author Andi Mathis, TU München,
+/// andreas.mathis@ph.tum.de \author Zuzanna Chochulska, WUT Warsaw,
+/// zchochul@cern.ch
 
 #ifndef PWGCF_FEMTOWORLD_CORE_FEMTOWORLDCOLLISIONSELECTION_H_
 #define PWGCF_FEMTOWORLD_CORE_FEMTOWORLDCOLLISIONSELECTION_H_
 
-#include <string>
 #include <iostream>
+#include <string>
 
 #include "Common/CCDB/TriggerAliases.h"
 #include "Framework/HistogramRegistry.h"
@@ -26,14 +27,13 @@
 
 using namespace o2::framework;
 
-namespace o2::analysis::femtoWorld
-{
+namespace o2::analysis::femtoWorld {
 
 /// \class FemtoWorldCollisionSelection
-/// \brief Small selection class to check whether a given collision fulfills the specified selections
-class FemtoWorldCollisionSelection
-{
- public:
+/// \brief Small selection class to check whether a given collision fulfills the
+/// specified selections
+class FemtoWorldCollisionSelection {
+public:
   /// Destructor
   virtual ~FemtoWorldCollisionSelection() = default;
 
@@ -42,8 +42,8 @@ class FemtoWorldCollisionSelection
   /// \param checkTrigger whether or not to check for the trigger alias
   /// \param trig Requested trigger alias
   /// \param checkOffline whether or not to check for offline selection criteria
-  void setCuts(float zvtxMax, bool checkTrigger, int trig, bool checkOffline, bool checkRun3)
-  {
+  void setCuts(float zvtxMax, bool checkTrigger, int trig, bool checkOffline,
+               bool checkRun3) {
     mCutsSet = true;
     mZvtxMax = zvtxMax;
     mCheckTrigger = checkTrigger;
@@ -54,23 +54,28 @@ class FemtoWorldCollisionSelection
 
   /// Initializes histograms for the task
   /// \param registry Histogram registry to be passed
-  void init(HistogramRegistry* registry)
-  {
+  void init(HistogramRegistry *registry) {
     if (!mCutsSet) {
       LOGF(error, "Event selection not set - quitting!");
     }
     mHistogramRegistry = registry;
-    mHistogramRegistry->add("Event/zvtxhist", "; vtx_{z} (cm); Entries", kTH1F, {{300, -12.5, 12.5}});
-    mHistogramRegistry->add("Event/MultV0M", "; vMultV0M; Entries", kTH1F, {{16384, 0, 32768}});
-    mHistogramRegistry->add("Event/MultT0M", "; vMultT0M; Entries", kTH1F, {{4096, 0, 8192}});
-    mHistogramRegistry->add("Event/MultNTracksPV", "; vMultNTracksPV; Entries", kTH1F, {{120, 0, 120}});
-    mHistogramRegistry->add("Event/MultNTracklets", "; vMultNTrackslets; Entries", kTH1F, {{300, 0, 300}});
-    mHistogramRegistry->add("Event/MultTPC", "; vMultTPC; Entries", kTH1I, {{600, 0, 600}});
+    mHistogramRegistry->add("Event/zvtxhist", "; vtx_{z} (cm); Entries", kTH1F,
+                            {{300, -12.5, 12.5}});
+    mHistogramRegistry->add("Event/MultV0M", "; vMultV0M; Entries", kTH1F,
+                            {{16384, 0, 32768}});
+    mHistogramRegistry->add("Event/MultT0M", "; vMultT0M; Entries", kTH1F,
+                            {{4096, 0, 8192}});
+    mHistogramRegistry->add("Event/MultNTracksPV", "; vMultNTracksPV; Entries",
+                            kTH1F, {{120, 0, 120}});
+    mHistogramRegistry->add("Event/MultNTracklets",
+                            "; vMultNTrackslets; Entries", kTH1F,
+                            {{300, 0, 300}});
+    mHistogramRegistry->add("Event/MultTPC", "; vMultTPC; Entries", kTH1I,
+                            {{600, 0, 600}});
   }
 
   /// Print some debug information
-  void printCuts()
-  {
+  void printCuts() {
     LOG(info) << "Debug information for FemtoDreamCollisionSelection";
     LOG(info) << "Max. z-vertex: " << mZvtxMax;
     LOG(info) << "Check trigger: " << mCheckTrigger;
@@ -83,9 +88,7 @@ class FemtoWorldCollisionSelection
   /// \tparam T type of the collision
   /// \param col Collision
   /// \return whether or not the collisions fulfills the specified selections
-  template <typename T>
-  bool isSelected(T const& col)
-  {
+  template <typename T> bool isSelected(T const &col) {
     if (std::abs(col.posZ()) > mZvtxMax) {
       return false;
     }
@@ -107,19 +110,22 @@ class FemtoWorldCollisionSelection
   /// Some basic QA of the event
   /// \tparam T type of the collision
   /// \param col Collision
-  template <typename T>
-  void fillQA(T const& col)
-  {
+  template <typename T> void fillQA(T const &col) {
     if (mHistogramRegistry) {
       mHistogramRegistry->fill(HIST("Event/zvtxhist"), col.posZ());
       mHistogramRegistry->fill(HIST("Event/MultT0M"), col.multFT0M());
-      mHistogramRegistry->fill(HIST("Event/MultNTracksPV"), col.multNTracksPV());
-      mHistogramRegistry->fill(HIST("Event/MultNTracklets"), col.multTracklets());
+      mHistogramRegistry->fill(HIST("Event/MultNTracksPV"),
+                               col.multNTracksPV());
+      mHistogramRegistry->fill(HIST("Event/MultNTracklets"),
+                               col.multTracklets());
       mHistogramRegistry->fill(HIST("Event/MultTPC"), col.multTPC());
       if (mCheckIsRun3) {
         mHistogramRegistry->fill(HIST("Event/MultV0M"), col.multFV0M());
       } else {
-        mHistogramRegistry->fill(HIST("Event/MultV0M"), 0.5 * (col.multFV0M())); // in AliPhysics, the VOM was defined by (V0A + V0C)/2.
+        mHistogramRegistry->fill(
+            HIST("Event/MultV0M"),
+            0.5 * (col.multFV0M())); // in AliPhysics, the VOM was defined by
+                                     // (V0A + V0C)/2.
       }
     }
   }
@@ -134,20 +140,19 @@ class FemtoWorldCollisionSelection
   /// \param tracks All tracks
   /// \return value of the sphericity of the event
   template <typename T1, typename T2>
-  float computeSphericity(T1 const& col, T2 const& tracks)
-  {
+  float computeSphericity(T1 const &col, T2 const &tracks) {
     return 2.f;
   }
 
- private:
-  HistogramRegistry* mHistogramRegistry = nullptr; ///< For QA output
-  bool mCutsSet = false;                           ///< Protection against running without cuts
-  bool mCheckTrigger = false;                      ///< Check for trigger
-  bool mCheckOffline = false;                      ///< Check for offline criteria (might change)
-  bool mCheckIsRun3 = false;                       ///< Check if running on Pilot Beam
-  triggerAliases mTrigger = kINT7;                 ///< Trigger to check for
-  float mZvtxMax = 999.f;                          ///< Maximal deviation from nominal z-vertex (cm)
+private:
+  HistogramRegistry *mHistogramRegistry = nullptr; ///< For QA output
+  bool mCutsSet = false;      ///< Protection against running without cuts
+  bool mCheckTrigger = false; ///< Check for trigger
+  bool mCheckOffline = false; ///< Check for offline criteria (might change)
+  bool mCheckIsRun3 = false;  ///< Check if running on Pilot Beam
+  triggerAliases mTrigger = kINT7; ///< Trigger to check for
+  float mZvtxMax = 999.f; ///< Maximal deviation from nominal z-vertex (cm)
 };
 } // namespace o2::analysis::femtoWorld
 
-#endif  // PWGCF_FEMTOWORLD_CORE_FEMTOWORLDCOLLISIONSELECTION_H_
+#endif // PWGCF_FEMTOWORLD_CORE_FEMTOWORLDCOLLISIONSELECTION_H_
